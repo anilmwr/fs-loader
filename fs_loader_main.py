b574e57b84f5d81fa3dd75ef5b8d84fb0d21033a
Anil Waghmare <anilmwr@gmail.com>
2023-05-01 22:27:50 +0530

    Resolve style issues reported by Flake8

--- fs_loader_main.py
+++ fs_loader_main.py
@@ -5,12 +5,17 @@ import argparse
 from fsutil.fs_loader import FileSystemLoader
 
 
-def main(mount_directory, sub_directory_depth, file_count_per_directory, sub_directory_count, directory_prefix,
-         block_size, min_block_count, max_block_count):
+def main(mount_directory, sub_directory_depth, file_count_per_directory,
+         sub_directory_count, directory_prefix, block_size, min_block_count,
+         max_block_count):
     try:
-        fs_loader = FileSystemLoader(mount_directory, sub_directory_depth, file_count_per_directory,
-                                  sub_directory_count=sub_directory_count, directory_prefix=directory_prefix,
-                                  block_size=block_size, min_block_count=min_block_count, max_block_count=max_block_count)
+        fs_loader = FileSystemLoader(mount_directory, sub_directory_depth,
+                                     file_count_per_directory,
+                                     sub_directory_count=sub_directory_count,
+                                     directory_prefix=directory_prefix,
+                                     block_size=block_size,
+                                     min_block_count=min_block_count,
+                                     max_block_count=max_block_count)
         fs_loader.load_fs()
     except KeyboardInterrupt:
         print('Interrupted by user')
@@ -22,15 +27,29 @@ def main(mount_directory, sub_directory_depth, file_count_per_directory, sub_dir
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mount-directory', type=str, help='Mount directory path', required=True)
-    parser.add_argument('--sub-directory-depth', type=int, help='Sub directory tree depth', required=False, default=5)
-    parser.add_argument('--file-count-per-directory', type=int, help='File count per directory', required=False, default=10)
-    parser.add_argument('--sub-directory-count', type=int, help='Sub-directory count', required=False, default=10)
-    parser.add_argument('--directory-prefix', type=str, help='Directory name prefix', required=False, default='dir')
-    parser.add_argument('--block-size', type=int, help='Block size for write', required=False, default=4096)
-    parser.add_argument('--min-block-count', type=int, help='Minimum number of blocks to write to each file', required=False, default=1)
-    parser.add_argument('--max-block-count', type=int, help='Maximum number of block to write to each file', required=False, default=256)
-    
+    parser.add_argument('--mount-directory', type=str,
+                        help='Mount directory path', required=True)
+    parser.add_argument('--sub-directory-depth', type=int,
+                        help='Sub directory tree depth', required=False,
+                        default=5)
+    parser.add_argument('--file-count-per-directory', type=int,
+                        help='File count per directory', required=False,
+                        default=10)
+    parser.add_argument('--sub-directory-count', type=int,
+                        help='Sub-directory count', required=False, default=10)
+    parser.add_argument('--directory-prefix', type=str,
+                        help='Directory name prefix', required=False,
+                        default='dir')
+    parser.add_argument('--block-size', type=int,
+                        help='Block size for write', required=False,
+                        default=4096)
+    parser.add_argument('--min-block-count', type=int,
+                        help='Minimum number of blocks to write to each file',
+                        required=False, default=1)
+    parser.add_argument('--max-block-count', type=int,
+                        help='Maximum number of block to write to each file',
+                        required=False, default=256)
+
     args = parser.parse_args()
     mount_directory = args.mount_directory
     sub_directory_depth = args.sub_directory_depth
@@ -44,15 +63,17 @@ if __name__ == '__main__':
     if directory_prefix == '':
         print('Directory prefix is empty')
         sys.exit(1)
-    
-    if any(i <= 0 for i in [sub_directory_depth, file_count_per_directory, sub_directory_count, block_size,
-                            min_block_count, max_block_count]):
+
+    if any(i <= 0 for i in [sub_directory_depth, file_count_per_directory,
+                            sub_directory_count, block_size, min_block_count,
+                            max_block_count]):
         print('Positive integer argument expected')
         sys.exit(1)
-    
+
     if max_block_count < min_block_count:
         print('Max block count cannot be less than min block count')
         sys.exit(1)
 
-    main(mount_directory, sub_directory_depth, file_count_per_directory, sub_directory_count, directory_prefix,
-         block_size, min_block_count, max_block_count)
+    main(mount_directory, sub_directory_depth, file_count_per_directory,
+         sub_directory_count, directory_prefix, block_size, min_block_count,
+         max_block_count)
--- fsutil/fs_loader.py
+++ fsutil/fs_loader.py
@@ -17,28 +17,34 @@ logger = logging.getLogger('FS-Loader')
 
 
 class FileSystemLoader:
-    def __init__(self, mount_directory: str, sub_directory_depth: int, file_count_per_directory: int,
-                 sub_directory_count: int = 10, block_size: int = 4096, min_block_count: int = 1,
-                 max_block_count: int = 256, directory_prefix='dir') -> None:
+    def __init__(self, mount_directory: str, sub_directory_depth: int,
+                 file_count_per_directory: int,
+                 sub_directory_count: int = 10, block_size: int = 4096,
+                 min_block_count: int = 1, max_block_count: int = 256,
+                 directory_prefix='dir') -> None:
         # check if mount directory exists
         if os.path.isdir(mount_directory):
             # check if mount directory is writable
             if os.access(mount_directory, os.W_OK | os.X_OK):
                 self.mount_directory = mount_directory
             else:
-                raise ValueError(f'Mount directory exists but not writable: {mount_directory}')
+                raise ValueError(f'Mount directory exists but not writable: \
+                                 {mount_directory}')
         else:
-            raise ValueError(f'Mount directory doesn\'t exist: {mount_directory}')
+            raise ValueError(f'Mount directory doesn\'t exist: \
+                             {mount_directory}')
 
         if directory_prefix == '':
             raise ValueError('Directory prefix is empty')
-        
+
         # check if rest of the arguments are positive integers
-        for parameter in [sub_directory_depth, file_count_per_directory, sub_directory_count,
-                          block_size, min_block_count, max_block_count]:
+        for parameter in [sub_directory_depth, file_count_per_directory,
+                          sub_directory_count, block_size, min_block_count,
+                          max_block_count]:
             if not isinstance(parameter, int) or parameter <= 0:
-                raise ValueError(f'Positive integer expected, received: {repr(parameter)}')
-        
+                raise ValueError(f'Positive integer expected, received: \
+                                 {repr(parameter)}')
+
         self.sub_directory_depth = sub_directory_depth
         self.file_count_per_directory = file_count_per_directory
         self.sub_directory_count = sub_directory_count
@@ -53,61 +59,77 @@ class FileSystemLoader:
         t = threading.Thread(target=self.display_progress, daemon=True)
         t.start()
 
-        for directory_ids in itertools.product(range(1, self.sub_directory_count + 1), repeat=self.sub_directory_depth):
+        for directory_ids in itertools.product(
+                range(1, self.sub_directory_count + 1),
+                repeat=self.sub_directory_depth):
             # recursive call to create sub directory tree
             current_directory_path = self.mount_directory
 
             # generate path for sub directory with all in-between directories
             for i in range(1, len(directory_ids) + 1):
-                sub_dir_name = os.path.join(current_directory_path, self.directory_prefix + '_'.join(map(str, directory_ids[:i])))
-                current_directory_path = os.path.join(current_directory_path, sub_dir_name)
+                sub_dir_name = os.path.join(
+                    current_directory_path, self.directory_prefix +
+                    '_'.join(map(str, directory_ids[:i])))
+                current_directory_path = os.path.join(
+                    current_directory_path, sub_dir_name)
 
             try:
                 os.makedirs(current_directory_path)
             except IOError as e:
-                logging.error(f'Unable to create directory {current_directory_path}: {str(e)}')
+                logging.error(f'Unable to create directory \
+                              {current_directory_path}: {str(e)}')
                 return
 
             # repeat for number of files to be created in sub directory
             for file_index in range(1, self.file_count_per_directory + 1):
-                current_file_path = os.path.join(current_directory_path, str(file_index) + '.txt')
+                current_file_path = os.path.join(current_directory_path,
+                                                 str(file_index) + '.txt')
 
-                # get random integer in the range min_block_count and max_block_count inclusive
-                file_block_count = random.randint(self.min_block_count, self.max_block_count)
+                # get random integer in the range min_block_count
+                # and max_block_count inclusive
+                file_block_count = random.randint(self.min_block_count,
+                                                  self.max_block_count)
 
                 try:
                     # open, write and close file
-                    with open(current_file_path, 'w', encoding='utf-8') as file_handle:
+                    with open(current_file_path, 'w', encoding='utf-8') as fh:
                         # repeat for number of blocks
                         for _ in range(file_block_count):
                             # create random text of block size
                             text_block = ''.join(random.choice(
-                                    string.ascii_uppercase + string.ascii_lowercase + string.digits
+                                    string.ascii_uppercase
+                                    + string.ascii_lowercase
+                                    + string.digits
                                 ) for _ in range(self.block_size))
-                            
+
                             # write text block to file handle
-                            file_handle.write(text_block)
-                    
+                            fh.write(text_block)
+
                     # increment file count for progress
                     self.current_file_count += 1
                 except IOError as e:
-                    logging.error(f'Unable to create file {current_file_path}: {str(e)}')
+                    logging.error(f'Unable to create file '
+                                  f'{current_file_path}: {str(e)}')
                     return
-        
+
         print('\x1b[1K\rCompleted')
-    
+
     def display_progress(self) -> None:
         progress_percent = 0
         file_count_tobe_created = self.get_file_count_tobe_created()
-        print(f'\rFiles created: {self.current_file_count:10d} Completed: {progress_percent:3d}%', end='')
+        print(f'\rFiles created: {self.current_file_count:12d} Completed: '
+              f'{progress_percent:3d}%', end='')
 
         while (progress_percent < 100):
-            print(f'\rFiles created: {self.current_file_count:10d} Completed: {progress_percent:3d}%', end='')
-            current_progress_percent = int((self.current_file_count / file_count_tobe_created) * 100)
+            print(f'\rFiles created: {self.current_file_count:12d} Completed: '
+                  f'{progress_percent:3d}%', end='')
+            current_progress_percent = \
+                int((self.current_file_count / file_count_tobe_created) * 100)
             if (current_progress_percent > progress_percent):
                 progress_percent = current_progress_percent
-            
+
             time.sleep(1)
-    
+
     def get_file_count_tobe_created(self) -> int:
-        return (self.sub_directory_count ** self.sub_directory_depth) * self.file_count_per_directory
+        return (self.sub_directory_count ** self.sub_directory_depth) \
+            * self.file_count_per_directory
